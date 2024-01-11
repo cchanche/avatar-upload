60f63f8fe712f17d18cc87bd0cf586066b846f8f
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2024-01-11 10:38:03 +0100

    chore: update bundle

--- dist/index.cjs
+++ dist/index.cjs
@@ -151,16 +151,16 @@ const createBadge = async (params, options) => {
 
 const getColorScore = (params) => {
     if (params.metric === 'happy') {
-        const hsv = rgb2hsl(params.color[0], params.color[1], params.color[2]);
+        const hsl = rgb2hsl(params.color[0], params.color[1], params.color[2]);
         // Hue score (warmness)
         const bestWarmnessHue = 0;
         const worstWarmnessHue = 180;
-        const closeToBestBy = Math.min(Math.abs(hsv.h - 360 + bestWarmnessHue), Math.abs(hsv.h + bestWarmnessHue));
+        const closeToBestBy = Math.min(Math.abs(hsl.h - 360 + bestWarmnessHue), Math.abs(hsl.h + bestWarmnessHue));
         const warmScore = 1 - closeToBestBy / worstWarmnessHue;
         // Saturation score (saturated colors are happier)
-        const satScore = hsv.s;
+        const satScore = hsl.s;
         // Lightness score (not too bright & not too dark colors are happier)
-        const lumScore = 1 - Math.abs((hsv.l - 0.5) / 0.5);
+        const lumScore = 1 - Math.abs((hsl.l - 0.5) / 0.5);
         return (warmScore + satScore + lumScore) / 3;
     }
     else
