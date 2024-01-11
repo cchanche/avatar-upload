d6e5ecd6e22887bc584df29d2f03a2a1f3a63850
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2024-01-11 10:37:22 +0100

    docs: rename hsv variable to the appropriate hsl name

--- src/core/get-color-score.ts
+++ src/core/get-color-score.ts
@@ -5,24 +5,24 @@ export const getColorScore = (params: {
   metric?: 'happy';
 }) => {
   if (params.metric === 'happy') {
-    const hsv = rgb2hsl(params.color[0], params.color[1], params.color[2]);
+    const hsl = rgb2hsl(params.color[0], params.color[1], params.color[2]);
 
     // Hue score (warmness)
     const bestWarmnessHue = 0;
     const worstWarmnessHue = 180;
 
     const closeToBestBy = Math.min(
-      Math.abs(hsv.h - 360 + bestWarmnessHue),
-      Math.abs(hsv.h + bestWarmnessHue),
+      Math.abs(hsl.h - 360 + bestWarmnessHue),
+      Math.abs(hsl.h + bestWarmnessHue),
     );
 
     const warmScore = 1 - closeToBestBy / worstWarmnessHue;
 
     // Saturation score (saturated colors are happier)
-    const satScore = hsv.s;
+    const satScore = hsl.s;
 
     // Lightness score (not too bright & not too dark colors are happier)
-    const lumScore = 1 - Math.abs((hsv.l - 0.5) / 0.5);
+    const lumScore = 1 - Math.abs((hsl.l - 0.5) / 0.5);
 
     return (warmScore + satScore + lumScore) / 3;
   } else return 0;
